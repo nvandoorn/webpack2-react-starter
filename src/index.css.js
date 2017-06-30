63425ce64546bed4eea22dd763dd8f48c2f3c330
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-06-29 23:29:15 -0700

    Test out new PostCSS config

--- src/index.css.js
+++
@@ -1,4 +0,0 @@
-export const app = {
-  fontSize: '40px',
-  display: 'flex'
-}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import FA from 'react-fontawesome'
 
 import { app } from './index.css'
+import '../node_modules/font-awesome/css/font-awesome.css'
 
-const App = p => <div className={app}>{p.text}</div>
+const App = p =>
+  <div className={app}>
+    <FA name='github' />
+    {p.text}
+  </div>
 
 ReactDOM.render(<App text='hello world' />, document.getElementById('root'))
